b2f796c215d7e610b3cb7b36cc93e1419b0c2b49
Aditya Pai <pai.a@somaiya.edu>
2024-07-30 14:15:01 +0530

    File Rename Backend Trial

--- backend/index.py
+++ backend/index.py
@@ -92,5 +92,7 @@ def get_random_sentence(n):
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+app = app
