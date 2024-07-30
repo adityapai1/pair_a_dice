1985504ce6b3214eb41c541751f99910a73cd43f
Aditya Pai <pai.a@somaiya.edu>
2024-07-30 13:56:05 +0530

    Added vercel.json

--- backend/flasker.py
+++ backend/flasker.py
@@ -89,5 +89,8 @@ def get_random_sentence(n):
         "strength": strength
     })
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
